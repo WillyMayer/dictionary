eb8b4ccb921ec900c985797f36882570cd5b5cbc
WillyMayer <wil.mayer.pulido@gmail.com>
2024-01-14 21:57:22 -0300

    preparing tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
+import Word from "./components/Word";
+import Footer from "./components/Footer";
+
 function App() {
 
   return (
     <>
-      <h1 className="header text-3xl">Hello World!</h1>
+      <Router>
+        <nav>
+          <Search />
+        </nav>
+        <Routes>
+          <Route path="/dictionary/" element={<Word />}/>
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
