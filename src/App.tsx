9ef47d8644a13ee5d99b702907639e614f56f3e2
WillyMayer <wil.mayer.pulido@gmail.com>
2024-01-14 23:01:58 -0300

    header & footer ready

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import Word from "./components/Word";
 import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <>
+    <div className="body bg-cyan-900">
       <Router>
         <nav>
           <Search />
         </nav>
         <Routes>
-          <Route path="/dictionary/" element={<Word />}/>
+          <Route path="/dictionary/" element={<Word />} />
         </Routes>
         <Footer />
       </Router>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
